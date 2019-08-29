723847f9dbcd65f69e28c0b586f82538c867b5e6
Maddi Joyce <maddi@maddijoyce.com>
2019-08-29 13:50:04 +1000

    Add components option.

--- index.d.ts
+++ index.d.ts
@@ -1,33 +1,34 @@
 interface GoogleProps {
-  types?: '(cities)' | 'geocode' | 'establishments' | 'address'
-  language?: string
-  location?: ''
-  radius?: number
-  strictbounds?: boolean
-  offset?: number
+  types?: "(cities)" | "geocode" | "establishments" | "address";
+  language?: string;
+  location?: string;
+  radius?: number;
+  strictbounds?: boolean;
+  offset?: number;
+  components?: string;
 }
 
 export interface AutocompleteProps {
-  apiKey: string
-  query: string
-  type?: 'places' | 'query'
-  debounceMs?: number
-  options?: GoogleProps
+  apiKey: string;
+  query: string;
+  type?: "places" | "query";
+  debounceMs?: number;
+  options?: GoogleProps;
 }
 
 interface ReturnProps {
   results: {
-    predictions: any[]
-    status: string
-  }
-  isLoading: boolean
-  error: null | string
-  getPlaceDetails: any
-  cancelQuery: (id: string) => void
+    predictions: any[];
+    status: string;
+  };
+  isLoading: boolean;
+  error: null | string;
+  getPlaceDetails: any;
+  cancelQuery: (id: string) => void;
 }
 
-declare module 'use-google-autocomplete' {
+declare module "use-google-autocomplete" {
   export default function useGoogleAutocomplete(
     props: AutocompleteProps
-  ): ReturnProps
+  ): ReturnProps;
 }
